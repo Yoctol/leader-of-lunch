72ab9d2a106f24a5e853e002a232a99b92a76edd
etrex kuo <et284vu065k3@gmail.com>
2019-10-21 19:15:23 +0800

    update routes

--- src/lol/actions/index.js
+++ src/lol/actions/index.js
@@ -20,8 +20,6 @@ const voteDelete = require('./votes/delete')
 module.exports = async function Actions(context, {next}){
   return router([
     text(/menu|help|午餐隊長/, menu),
-    text(/我是?誰/, userShow),
-    text(/叫?我(?<name>.+)/, userUpdate),
     text(/餐廳/, restaurantIndex),
     text(/新增(?<name>.+)/, restaurantCreate),
     text(/刪除(?<name>.+)/, restaurantDelete),
@@ -29,6 +27,8 @@ module.exports = async function Actions(context, {next}){
     text(/^(餓|想?吃|午餐)/, electionCreate),
     text(/走|出發|統計/, electionShow),
     text(/^(1|2|3|4|one|two|three|four).*$/, voteCreate),
+    text(/我是?誰/, userShow),
+    text(/叫?我(?<name>.+)/, userUpdate),
     text("*", debug),
   ])
 }
