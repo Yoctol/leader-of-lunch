0d4c000c00116300b9468d3e604b5200b6476693
etrex kuo <et284vu065k3@gmail.com>
2019-10-21 15:52:49 +0800

    only debug in development mode

--- src/lol/actions/debug.js
+++ src/lol/actions/debug.js
@@ -1,4 +1,7 @@
 module.exports = async function Debug(context, args){
+  if(process.env.ENV != 'development'){
+    return;
+  }
   const event = context.event
   const event_json = JSON.stringify(event)
   console.log(event_json)
