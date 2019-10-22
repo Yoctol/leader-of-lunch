7f2fe84002e7043e8cc886e73cd2cfaa0741b9f7
etrex kuo <et284vu065k3@gmail.com>
2019-10-22 16:56:48 +0800

    feat: show menu when join or follow

--- src/lol/middlewares/line/addText.js
+++ src/lol/middlewares/line/addText.js
@@ -1,6 +1,10 @@
 module.exports = async function AddText(context, { next }){
   context.text = context.event.text || context.event.rawEvent.type
 
+  if(context.event.rawEvent.type == 'follow' || context.event.rawEvent.type == 'join'){
+    context.text = 'menu'
+  }
+
   // // default
   context.text = context.text || ''
   console.log(`set text: ${context.text}`)
