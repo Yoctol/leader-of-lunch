2dbee07da4b1eca50ec2856bccb5fd4f0132569c
etrex kuo <et284vu065k3@gmail.com>
2019-10-25 14:33:26 +0800

    chore: show rawEvent

--- src/lol/actions/debug.js
+++ src/lol/actions/debug.js
@@ -3,7 +3,5 @@ module.exports = async function Debug(context) {
     return;
   }
   const event = context.event;
-  const event_json = JSON.stringify(event);
-  console.log(event_json);
   await context.sendText(JSON.stringify(context.event));
 };
--- src/lol/index.js
+++ src/lol/index.js
@@ -2,6 +2,9 @@ const { chain } = require('bottender');
 const middlewares = require('./middlewares');
 const actions = require('./actions');
 
-module.exports = async function LOL() {
+module.exports = async function LOL(context) {
+  if(process.env.ENV == "development") {
+    console.log(context.event.rawEvent)
+  }
   return chain([middlewares, actions]);
 };
