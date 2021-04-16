fc7651ae29e30afa608cf6e119a8e44db56335af
etrex kuo <et284vu065k3@gmail.com>
2021-04-16 17:24:44 +0800

    retry connect to db

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import LOL from './lol';
 
-createConnection()
-  .then(async connection => {
+function connectToDatabase(){
+  createConnection()
+  .then(async _ => {
     console.log('Connection established.');
   })
-  .catch(error => console.log(error));
+  .catch(error => {
+    console.log(error)
+    setTimeout(connectToDatabase, 30000);
+  });
+}
 
+connectToDatabase()
 export default LOL;
